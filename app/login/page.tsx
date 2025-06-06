625afe3f567069c004fe171ce6d8868afaf16a0f
Mohammad-Aghai <mohamadaghai1381@gmail.com>
2025-06-06 21:21:23 +0330

    update 2

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,8 +1,5 @@
 "use client"
 import React from 'react'
-import { useContext } from 'react'
-import { IsLogin } from '@/contexts/IsUserLogin'
-import { UserLoginContext } from '@/contexts/IsUserLogin'
 import { userSignin } from '@/redux/usersSlice'
 import FormModal from '../components/FormModal'
 
